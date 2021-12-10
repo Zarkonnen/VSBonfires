25b41c71bce0073540d1479798e154c738142dce
Zarkonnen <david.stark@zarkonnen.com>
2021-12-10 15:52:34 +0100

    Initial setup

---
+++ src/HowtoExampleMod.cs
@@ -0,0 +1,29 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo( "Bonfires",
+	Description = "Adds Bonfires",
+	Website     = "https://github.com/copygirl/howto-example-mod",
+	Authors     = new []{ "Zarkonnen" } )]
+
+namespace Bonfires
+{
+	public class BonfiresMod : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			//api.RegisterBlockBehaviorClass(InstaTNTBehavior.NAME, typeof(InstaTNTBehavior));
+		}
+		
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			
+		}
+		
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			
+		}
+	}
+}
